098ed72de458e1708008208dc9b68794394cd425
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-09-04 19:01:58 +0200

    Coordinator Dashboard made

---
+++ AaliyahAllie_ST10212542_PROG6212_PART1/CoordinatorDashboard.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AaliyahAllie_ST10212542_PROG6212_PART1
+{
+    /// <summary>
+    /// Interaction logic for CoordinatorDashboard.xaml
+    /// </summary>
+    public partial class CoordinatorDashboard : Window
+    {
+        public CoordinatorDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}

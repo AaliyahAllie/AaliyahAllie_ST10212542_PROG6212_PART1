385f6bb9f2694096fd3c1fa122df6367cbe90ba5
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-09-04 18:42:26 +0200

    Add project files.

---
+++ AaliyahAllie_ST10212542_PROG6212_PART1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AaliyahAllie_ST10212542_PROG6212_PART1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
